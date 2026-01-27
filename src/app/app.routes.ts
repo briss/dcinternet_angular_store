c24f7de2295299752b9ad7e089adafdb736de98b
brissgs <briseidags@gmail.com>
2026-01-26 19:50:32 -0600

    Clase 12: Agregar barra de menú para mostrar los ejemplos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { ContadorComponent } from './contador/contador-component/contador-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: App },
+    { path: 'contador', component: ContadorComponent },
+    
+    { path: '**', component: App }
+];
